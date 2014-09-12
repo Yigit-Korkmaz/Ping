904f3a3967d36cff85933162191109a1bc29fe33
Yiğit Korkmaz <mkp.yigit@gmail.com>
2014-09-12 16:55:55 +0300

    Update Ping.bat

--- Ping.bat
+++ Ping.bat
@@ -21,70 +21,70 @@ goto Basla
 :Basla
 echo Islem Tamamlandi 10
 color a
-ping www.github.com
+ping www.google.com
 cls
 goto iki
 
 :iki
 echo Islem Tamamlandi 20
 color a
-ping www.github.com
+ping www.google.com
 cls
 goto uc
 
 :uc
 echo Islem Tamamlandi 30
 color a
-ping www.github.com
+ping www.google.com
 cls
 goto dort
 
 :dort
 echo Islem Tamamlandi 40
 color a 
-ping www.github.com
+ping www.google.com
 cls
 goto bes
 
 :bes
 echo Islem Tamamlandi 50
 color a
-ping www.github.com
+ping www.google.com
 cls
 goto alti
 
 :alti
 echo Islem Tamamlandi 60
 color a
-ping www.github.com
+ping www.google.com
 cls
 goto yedi
 
 :yedi
 echo Islem Tamamlandi 70
 color a
-ping www.github.com
+ping www.google.com
 cls
 goto sekiz
 
 :sekiz
 echo Islem Tamamlandi 80
 color a
-ping www.github.com
+ping www.google.com
 cls
 goto dokuz
 
 :dokuz
 echo Islem Tamamlandi 90
 color a
-ping www.github.com
+ping www.google.com
 cls
 goto yuz
 
 :yuz
 echo Islem Tamamlandi 100
 color a
-ping www.github.com
+ping www.google.com
 cls
 goto bitti
 
@@ -93,3 +93,4 @@ color a
 echo İslem basariyla tamamlandi.
 cls
 pause
+
