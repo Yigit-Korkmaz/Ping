0205b280195b1a0baee363259dc010d0e71c6efb
Yiğit Korkmaz <mkp.yigit@gmail.com>
2014-09-12 16:52:44 +0300

    Create Ping.bat

---
+++ Ping.bat
@@ -0,0 +1,95 @@
+@echo off
+color a
+cls
+goto onay
+
+:onay
+echo Ad:Ping
+echo Zarar: Yok
+echo Amaci: Sitelere veri paketi yollamak
+pause
+cls
+goto Islem
+
+:Islem
+echo Islem Baslatiliyor...
+color a
+pause
+cls
+goto Basla
+
+:Basla
+echo Islem Tamamlandi 10
+color a
+ping www.google.com
+cls
+goto iki
+
+:iki
+echo Islem Tamamlandi 20
+color a
+ping www.google.com
+cls
+goto uc
+
+:uc
+echo Islem Tamamlandi 30
+color a
+ping www.google.com
+cls
+goto dort
+
+:dort
+echo Islem Tamamlandi 40
+color a 
+ping www.google.com
+cls
+goto bes
+
+:bes
+echo Islem Tamamlandi 50
+color a
+ping www.google.com
+cls
+goto alti
+
+:alti
+echo Islem Tamamlandi 60
+color a
+ping www.google.com
+cls
+goto yedi
+
+:yedi
+echo Islem Tamamlandi 70
+color a
+ping www.google.com
+cls
+goto sekiz
+
+:sekiz
+echo Islem Tamamlandi 80
+color a
+ping www.google.com
+cls
+goto dokuz
+
+:dokuz
+echo Islem Tamamlandi 90
+color a
+ping www.google.com
+cls
+goto yuz
+
+:yuz
+echo Islem Tamamlandi 100
+color a
+ping www.google.com
+cls
+goto bitti
+
+:bitti
+color a
+echo İslem basariyla tamamlandi.
+cls
+pause
